50acd80da6f446db4cce886df99198d2f843f5f1
Jamie Hollern <jamiehollern@gmail.com>
2019-05-22 16:37:50 +0100

    SO8-188 | Removed markup processing events from module.

--- src/TwigProcessor.php
+++ src/TwigProcessor.php
@@ -19,44 +19,28 @@ class TwigProcessor {
   protected $twigEnvironment;
 
   /**
-   * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
-   */
-  protected $eventDispatcher;
-
-  /**
    * TwigProcessor constructor.
    *
    * @param \Drupal\Core\Template\TwigEnvironment $twig_environment
-   * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $event_dispatcher
    */
-  public function __construct(TwigEnvironment $twig_environment, EventDispatcherInterface $event_dispatcher) {
+  public function __construct(TwigEnvironment $twig_environment) {
     $this->twigEnvironment = $twig_environment;
-    $this->eventDispatcher = $event_dispatcher;
   }
 
   /**
    * Runs the contents of a Twig template through the Twig engine.
    *
-   * @param string $twig_markup
+   * @param string $file_path
    *
    * @return string
    * @throws \Twig\Error\LoaderError
    * @throws \Twig\Error\RuntimeError
    * @throws \Twig\Error\SyntaxError
    */
-  public function processTwigTemplate($twig_markup) {
-    // Preprocess.
-    $event = new ProcessTwigEvent($twig_markup);
-    $this->eventDispatcher->dispatch(ProcessTwigEvent::PREPROCESS, $event);
-    $twig_markup = $event->getTwigMarkup();
-    // Process.
-    $template = $this->twigEnvironment->load($twig_markup);
-    $twig_markup = $template->render();
-    // Post process.
-    $event = new ProcessTwigEvent($twig_markup);
-    $this->eventDispatcher->dispatch(ProcessTwigEvent::POSTPROCESS, $event);
-    $twig_markup = $event->getTwigMarkup();
-    return $twig_markup;
+  public function processTwigTemplate($file_path) {
+    $template = $this->twigEnvironment->load($file_path);
+    $markup = $template->render();
+    return $markup;
   }
 
 }
